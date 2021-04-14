c51ee0078ba05d180997455c91cf8eb58f1e29a8
Alexander K <niktonezapomnil@gmail.com>
2021-04-14 15:12:54 +0500

    google rec lazy loading

--- js/scripts.js
+++ js/scripts.js
@@ -216,3 +216,22 @@ $(document).ready(function () {
     }
     openAccordion();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-img"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
+  }
+});
